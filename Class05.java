b4277646e07c6c5323a0ec423cf4d8ca47ce8c4f
kaling3536 <107583599+kaling3536@users.noreply.github.com>
2022-10-28 20:11:13 +0800

    Create Class05.java

---
+++ Class05.java
@@ -0,0 +1,47 @@
+package test01;
+
+interface Math {
+	public void show();
+
+	public void add(int a, int b); // 計算 a+b
+
+	public void sub(int a, int b); // 計算 a-b
+
+	public void mul(int a, int b); // 計算 a*b
+
+	public void div(int a, int b); // 計算 a/b
+}
+
+class Compute implements Math {
+	public int ans;
+
+	public void add(int a, int b) {
+		ans = a + b;
+	}
+
+	public void sub(int a, int b) {
+		ans = a - b;
+	}
+
+	public void mul(int a, int b) {
+		ans = a * b;
+	}
+
+	public void div(int a, int b) {
+		ans = a / b;
+	}
+
+	public void show() {
+		System.out.println("ans=" + ans);
+	}
+}
+
+public class hw11_5
+ {
+ public static void main(String args[])
+ {
+ Compute cmp=new Compute();
+ cmp.mul(3,5); // 計算 3*5
+ cmp.show(); // 此行會回應"ans=15"字串
+ }
+ }
